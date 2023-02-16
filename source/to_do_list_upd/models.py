5b56f37839c6bcf1a81119beed94ddd806084028
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-02-16 23:38:55 +0600

    Detail description
    Добавил в модель задачи поле для подробного описания. Создал миграцию и применил её. Заполнил новое поле в существующих задачах описаниями и обновил фикстуру

--- source/to_do_list_upd/models.py
+++ source/to_do_list_upd/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     description = models.CharField(max_length=2000, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=200, null=False, blank=False, default='new', verbose_name='Статус')
     finish_date = models.DateField(auto_now=False, verbose_name='Дата выполнения')
+    detail_description = models.TextField(max_length=2000, null=True, blank=False, verbose_name='Подробное описание')
 
     def __str__(self):
         return f'{self.description}, {self.status}, {self.finish_date}'
